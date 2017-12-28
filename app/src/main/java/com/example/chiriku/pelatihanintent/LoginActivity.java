0fd20c5a5bcd93f34a69b880e152de53c66ebd78
alirhmat <chiriku.ali.rhmat@gmail.com>
2017-12-28 19:12:10 +0700

    remove ; in LoginActivity

--- app/src/main/java/com/example/chiriku/pelatihanintent/LoginActivity.java
+++ app/src/main/java/com/example/chiriku/pelatihanintent/LoginActivity.java
@@ -37,12 +37,11 @@ public class LoginActivity extends AppCompatActivity {
                     Intent intent = new Intent(LoginActivity.this , MainActivity.class);
                     startActivity(intent);
 
-
                 }
                 else {
 
                     editTxtUsername.setText("");
-                    editTxtPassword.setText("");
+                    editTxtPassword.setText("")
 
                 }
 
